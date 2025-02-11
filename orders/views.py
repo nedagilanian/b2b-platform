eca4f7c5d98675a2a52f936913f46dfe460971d3
nedagilanian <141037195+nedagilanian@users.noreply.github.com>
2025-02-11 09:47:01 +0000

    Add searching and filtering for orders

--- orders/views.py
+++ orders/views.py
@@ -1,11 +1,31 @@
 from rest_framework import viewsets, permissions
 from .models import Order, OrderItem,Payment 
 from .serializers import OrderSerializer, OrderItemSerializer, PaymentSerializer
+import django_filters
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
+
+
+
+class OrderFilter(django_filters.FilterSet):
+    status = django_filters.CharFilter(field_name='status', lookup_expr='icontains')
+    min_total_price = django_filters.NumberFilter(field_name='total_price', lookup_expr='gte')
+    max_total_price = django_filters.NumberFilter(field_name='total_price', lookup_expr='lte')
+    start_date = django_filters.DateFilter(field_name='created_at', lookup_expr='gte')
+    end_date = django_filters.DateFilter(field_name='created_at', lookup_expr='lte')
+
+    class Meta:
+        model = Order
+        fields = ['status', 'min_total_price', 'max_total_price', 'start_date', 'end_date']
+    
 
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
     permission_classes = [permissions.IsAuthenticated]
+    filter_backends = (DjangoFilterBackend,SearchFilter)
+    filterset_class = OrderFilter
+    search_fields = ['id', 'user__username']
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
